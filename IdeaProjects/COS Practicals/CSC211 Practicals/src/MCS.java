b244bd3ff3ea930dacb1a8257dcd8cf212e502d8
aaronjf-git <aaron.fortun9@gmail.com>
2026-02-16 15:29:19 +0200

    Fixed index out of bounds error

--- IdeaProjects/COS Practicals/CSC211 Practicals/src/MCS.java
+++ IdeaProjects/COS Practicals/CSC211 Practicals/src/MCS.java
@@ -59,11 +59,13 @@ public class MCS {
            return maxsofar
          */
         int n = X.length;
-        int[] sumTo = new int[n];
-        for (int i = 0; i < n; i++)
+        int[] sumTo = new int[n+1];
+        for (int j = 0; j < sumTo.length; j++)
+            sumTo[j] = 0;
+        for (int i = 1; i < n; i++)
             sumTo[i] = sumTo[i-1] + X[i];
         int maxsofar = 0;
-        for (int low = 0; low < n; low++){
+        for (int low = 1; low < n; low++){
             for (int high = low; high < n; high++){
                 int sum = sumTo[high] - sumTo[low-1];
                 if (sum > maxsofar)
@@ -73,12 +75,26 @@ public class MCS {
         return maxsofar;
     }
 
+    public static int mcsOn(int [] X){
+        /*
+        def mcsOn(X):
+            N = len(X)
+            maxSoFar = 0.0
+            maxToHere = 0.0
+            for i in range(N):           # [1,N)
+               maxToHere = max(maxToHere +X[i], 0.0)
+               maxSoFar = max(maxSoFar, maxToHere)
+            return maxSoFar
+         */
+    }
+
 
 
     public static void main(String[] args){
         int[] X = {-5, 46, 89, 21, -9, -30, -100};
-        System.out.println("Order n^3 output: ");
-        System.out.println(mcsOn3(X));
-        System.out.printf("Order n^2 A output: %d", mcsOn2B(X));
+        System.out.printf("Order n^3 output: %d\n",mcsOn3(X));
+        System.out.printf("Order n^2 A output: %d\n", mcsOn2A(X));
+        System.out.printf("Order n^2 B output: %d\n", mcsOn2B(X));
+        System.out.print();
     }
 }
