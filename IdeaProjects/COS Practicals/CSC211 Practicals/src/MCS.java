92c48654dca5b7a3a7da4890dceef7d404e5672d
aaronjf-git <aaron.fortun9@gmail.com>
2026-02-16 15:11:13 +0200

    Fixed previous function names a.w.a translated python mcsO(n^2)B algorithm to java

--- IdeaProjects/COS Practicals/CSC211 Practicals/src/MCS.java
+++ IdeaProjects/COS Practicals/CSC211 Practicals/src/MCS.java
@@ -16,7 +16,7 @@ public class MCS {
         return maxsofar;
     }
 
-    public static int mscOn2A( int[] X){
+    public static int mcsOn2A( int[] X){
         /*
         def mcsOn2A(X):
            n = len(X)
@@ -42,11 +42,43 @@ public class MCS {
         return maxsofar;
     }
 
-    
+    public static int mcsOn2B(int [] X){
+        /*
+        def mcsOn2B(X):
+           n = len(X)
+           sumTo = [0]*(n+1)
+           for i in range(n):               # [0,n)
+              sumTo[i] = sumTo[i-1] + X[i]
+           maxsofar = 0
+           for low in range(n):             # [0,n)
+               for high in range(low, n):   # [low,n)
+                  sum = sumTo[high] - sumTo[low-1]
+                  # sum of all elements in X[low..high)
+                  if (sum > maxsofar):
+                     maxsofar = sum
+           return maxsofar
+         */
+        int n = X.length;
+        int[] sumTo = new int[n];
+        for (int i = 0; i < n; i++)
+            sumTo[i] = sumTo[i-1] + X[i];
+        int maxsofar = 0;
+        for (int low = 0; low < n; low++){
+            for (int high = low; high < n; high++){
+                int sum = sumTo[high] - sumTo[low-1];
+                if (sum > maxsofar)
+                    maxsofar = sum;
+            }
+        }
+        return maxsofar;
+    }
+
+
 
     public static void main(String[] args){
         int[] X = {-5, 46, 89, 21, -9, -30, -100};
-        //O(n^3)
+        System.out.println("Order n^3 output: ");
         System.out.println(mcsOn3(X));
+        System.out.printf("Order n^2 A output: %d", mcsOn2B(X));
     }
 }
