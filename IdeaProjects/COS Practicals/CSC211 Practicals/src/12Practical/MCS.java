6e79c9d25d89b011451d078d200ea507cff25b39
aaronjf-git <aaron.fortun9@gmail.com>
2026-02-16 16:23:36 +0200

    removed print statements for function outputs

---
+++ IdeaProjects/COS Practicals/CSC211 Practicals/src/12Practical/MCS.java
@@ -0,0 +1,140 @@
+//12Practical
+
+public class MCS {
+    public static int mcsOn3(int[] X){
+        int n = X.length;
+        int maxsofar = 0;
+        for (int low = 0; low < n; low++){
+            for (int high = low; high < n; high++){
+                int sum = 0;
+                for (int r = low; r <= high;r++){
+                    sum += X[r];
+                    if (sum > maxsofar)
+                        maxsofar = sum;
+                }
+            }
+        }
+        return maxsofar;
+    }
+
+    public static int mcsOn2A( int[] X){
+        /*
+        def mcsOn2A(X):
+           n = len(X)
+           maxsofar = 0
+           for low in range(n):             # [0,n)
+               sum = 0
+               for r in range(low, n):   # [low,n)
+                  sum += X[r]
+                  if (sum > maxsofar):
+                     maxsofar = sum
+           return maxsofar
+         */
+        int n = X.length;
+        int maxsofar = 0;
+        for (int low = 0; low < n; low++){
+            int sum = 0;
+            for (int r = low; r < n; r++){
+                sum += X[r];
+                if (sum > maxsofar)
+                    maxsofar = sum;
+            }
+        }
+        return maxsofar;
+    }
+
+    public static int mcsOn2B(int [] X){
+        /*
+        def mcsOn2B(X):
+           n = len(X)
+           sumTo = [0]*(n+1)
+           for i in range(n):               # [0,n)
+              sumTo[i] = sumTo[i-1] + X[i]
+           maxsofar = 0
+           for low in range(n):             # [0,n)
+               for high in range(low, n):   # [low,n)
+                  sum = sumTo[high] - sumTo[low-1]
+                  # sum of all elements in X[low..high)
+                  if (sum > maxsofar):
+                     maxsofar = sum
+           return maxsofar
+         */
+        int n = X.length;
+        int[] sumTo = new int[n+1];
+        for (int j = 0; j < sumTo.length; j++)
+            sumTo[j] = 0;
+        for (int i = 1; i < n; i++)
+            sumTo[i] = sumTo[i-1] + X[i];
+        int maxsofar = 0;
+        for (int low = 1; low < n; low++){
+            for (int high = low; high < n; high++){
+                int sum = sumTo[high] - sumTo[low-1];
+                if (sum > maxsofar)
+                    maxsofar = sum;
+            }
+        }
+        return maxsofar;
+    }
+
+    public static int mcsOn(int [] X){
+        /*
+        def mcsOn(X):
+            N = len(X)
+            maxSoFar = 0.0
+            maxToHere = 0.0
+            for i in range(N):           # [1,N)
+               maxToHere = max(maxToHere +X[i], 0.0)
+               maxSoFar = max(maxSoFar, maxToHere)
+            return maxSoFar
+         */
+        int n = X.length;
+        int maxSoFar = 0;
+        int maxToHere = 0;
+        for (int i = 1; i < n; i++){
+            maxToHere = Math.max(maxToHere + X[i], 0);
+            maxSoFar = Math.max(maxSoFar, maxToHere);
+        }
+        return maxSoFar;
+    }
+
+
+    public static void main(String[] args){
+        /*
+        def main():
+            n = 20
+            X = [randint(1,n)*(-1)**randint(2,4) for k in range(n)]
+            countP = 0
+            countM = 0
+            for x in X:
+                if x < 0:
+                    countM +=1
+                else:
+                    countP +=1
+            print(f"{countM = }  {countP = }")
+         */
+        int n = 20; //X length
+        int[] X = new int[n];
+        int negNums = Math.floorDiv(n, 3);
+        int r = (int)(Math.random()*n) + 1;
+        int negative_r = (int)(Math.random()*n - 1);
+        for (int i = 0; i < n-negNums; i++)
+            X[i] = r;
+        for (int j = negNums+1; j < n; j++)
+            X[j] = negative_r;
+
+        int countP = 0;
+        int countM = 0;
+
+        for (int x = 0; x <= n; x++){
+            if (x < 0)
+                countM += 1;
+            countP += 1;
+        }
+        System.out.printf("CountP = %d CountM = %d\n", countP, countM);
+
+        System.out.printf("Order n^3 output: %d\n",mcsOn3(X));
+        System.out.printf("Order n^2 A output: %d\n", mcsOn2A(X));
+        System.out.printf("Order n^2 B output: %d\n", mcsOn2B(X));
+        System.out.printf("Order n output: %d", mcsOn(X));
+    }
+}
